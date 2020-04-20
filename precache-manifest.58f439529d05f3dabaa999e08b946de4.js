1014e1bdc7a55a935ee2dba2af97bb9af3b22847
ShruthiKannurSvmx <shruthi.kannur@servicemax.com>
2020-04-20 18:26:16 +0530

    Updates

---
+++ precache-manifest.58f439529d05f3dabaa999e08b946de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c288d6ea81f8928bc962569221e3480a",
+    "url": "/Add-To-Cart-Experience/index.html"
+  },
+  {
+    "revision": "0b72f8112b820c734832",
+    "url": "/Add-To-Cart-Experience/static/css/main.ce936353.chunk.css"
+  },
+  {
+    "revision": "8d5b2f388b27f0d5f493",
+    "url": "/Add-To-Cart-Experience/static/js/2.91d79a69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Add-To-Cart-Experience/static/js/2.91d79a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b72f8112b820c734832",
+    "url": "/Add-To-Cart-Experience/static/js/main.87448da0.chunk.js"
+  },
+  {
+    "revision": "9f7e009ba6e3a6ed2ce5",
+    "url": "/Add-To-Cart-Experience/static/js/runtime-main.85a11463.js"
+  }
+]);
